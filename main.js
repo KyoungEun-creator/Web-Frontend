79717c5c5d82d07048f510851025f15bf9de7b87
조경은 <kavvy99@naver.com>
2023-10-02 23:15:10 +0900

    youtube clone coding update

---
+++ main.js
@@ -0,0 +1,48 @@
+// // #nav-open-btn click event
+// document.querySelector('#nav-open-btn').addEventListener('click'), function () {
+//     console.log('배고프다')
+//     // if (document.querySelector('#explicit-nav').style.display == 'none') {
+//     //     document.querySelector('#explicit-nav').style.display = 'block';
+//     //     document.querySelector('#implicit-nav').style.display = 'none';
+//     // } else {
+//     //     document.querySelector('#detailed-nav').style.display = 'none';
+//     //     document.querySelector('#implicit-nav').style.display = 'block';
+//     // }
+// }
+
+// // header hover event
+// document.querySelector('#magnifyingGlass-btn').addEventListener('mouseover', function () {
+//     document.querySelector('#magnifyingGlass-hover').style.display = 'block';
+// });
+// document.querySelector('#magnifyingGlass-btn').addEventListener('mouseout', function () {
+//     document.querySelector('#magnifyingGlass-hover').style.display = 'none';
+// })
+// // document.querySelector('#microphone-btn').addEventListener('mouseover', function () {
+// //     document.querySelector('#microphone-hover').style.display = 'block';
+// // });
+// // document.querySelector('#microphone-btn').addEventListener('mouseout', function () {
+// //     document.querySelector('#microphone-hover').style.display = 'none';
+// // })
+// // document.querySelector('#upload-btn').addEventListener('mouseover', function () {
+// //     document.querySelector('#upload-hover').style.display = 'block';
+// // });
+// // document.querySelector('#upload-btn').addEventListener('mouseout', function () {
+// //     document.querySelector('#upload-hover').style.display = 'none';
+// // })
+// // document.querySelector('#alarm-btn').addEventListener('mouseover', function () {
+// //     document.querySelector('#alarm-hover').style.display = 'block';
+// // });
+// // document.querySelector('#alarm-btn').addEventListener('mouseout', function () {
+// //     document.querySelector('#alarm-hover').style.display = 'none';
+// // })
+
+
+// document.querySelector('#nav-open-btn').addEventListener('click', function () {
+//     if (document.querySelector('#explicit-nav').style.display == 'none') {
+//         document.querySelector('#explicit-nav').style.display = 'block';
+//         document.querySelector('#implicit-nav').style.display = 'none';
+//     } else if (document.querySelector('#explicit-nav').style.display == 'block') {
+//         document.querySelector('#detailed-nav').style.display = 'none';
+//         document.querySelector('#implicit-nav').style.display = 'block';
+//     }
+// });
